201863360c201572aeeec00c6003f5d47cc24684
idevin <Mr.wyw@foxmail.com>
2020-08-26 15:42:14 +0800

    add 70012, 70019

--- translator/rules.py
+++ translator/rules.py
@@ -13,5 +13,14 @@ Rules = {
         'fields': ["service_name", "ip", "end_time", "display_name", "type", "start_time", "service_type",
                    "branch_id", "opnum", "service_start_type", "group", "status", "binary_path_name"],
         'ts': 'start_time'
+    },
+    '70012': {
+        'fields': ["ip", "end_time", "days_of_month", "periodically", "type", "command", "days_of_week",
+                   "add_current_date", "start_time", "job_time", "branch_id", "group", "status"],
+        'ts': 'start_time'
+    },
+    '70019': {
+        'fields': ["start_time", "opnum", "branch_id", "ip", "end_time", "type", "group", "status", "object", "command"],
+        'ts': 'start_time'
     }
 }
